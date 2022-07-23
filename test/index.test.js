c4db08950aff292c5faef04805eede9f176e0686
Joe Hildebrand <joe-github@cursive.net>
2022-07-23 16:10:01 -0600

    Add visitor, tweak output AST

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ const { ESLint } = require("eslint");
 const assert = require("assert");
 const path = require("path");
 const test = require("node:test");
+const { parseForESLint } = require("../lib/index");
 
 test("parse", async() => {
   const esl = new ESLint({
@@ -19,4 +20,12 @@ test("parse", async() => {
   assert.equal(report[0].warningCount, 0);
   assert.deepEqual(report[0].messages, []);
   assert.deepEqual(report[0].suppressedMessages, []);
+});
+
+test("error", () => {
+  assert.throws(() => parseForESLint("foo='"), /SyntaxError: Expected/);
+  assert.throws(
+    () => parseForESLint("foo='", { filePath: "bar" }),
+    /File: "bar"/
+  );
 });
